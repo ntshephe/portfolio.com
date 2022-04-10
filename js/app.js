b19d0b951107c854d3680af6dc4c7333c2cd3684
ntshephe <77022451+ntshephe@users.noreply.github.com>
2022-04-10 19:11:38 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ const skills_wrap = document.querySelector(".skills");
 const skills_bars = document.querySelectorAll(".skill-progress");
 const records_wrap = document.querySelector(".records");
 const records_numbers = document.querySelectorAll(".number");
-const footer_input = document.querySelector(".footer-input");
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector("header nav");
 const links = document.querySelectorAll(".links a");
@@ -85,15 +84,6 @@ function showSkills() {
 
     }
 }
-
-footer_input.addEventListener("focus", () => {
-    footer_input.classList.add("focus");
-})
-
-footer_input.addEventListener("blur", () => {
-    if (footer_input.value != "") return;
-    footer_input.classList.remove("focus");
-})
 
 function closeMenu() {
     navbar.classList.remove("open");
